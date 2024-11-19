5e35f700312f7fef85161b43e56b04df2947eeca
Plyassunova <alenaplasunovaa@gmail.com>
2024-11-19 11:10:23 +0600

    update

--- App.py
+++ App.py
@@ -1,34 +1,55 @@
+import os.path
+
 import streamlit as st
+from rich.jupyter import display
 from streamlit_navigation_bar import st_navbar as navbar
+from streamlit_navigation_bar.example import logo_path
+
 from Pages import Home, Project1, Project2 , Project3
+from PIL import Image
+import pandas as pd
+import numpy as np
 
-st.set_page_config(initial_sidebar_state="collapsed")
 
+image = Image.open("img/kotik.png")
+st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
+logo_path = os.path.join(os.path.dirname(__file__), "img", "kotik (1).svg")
 pages = ["Home","Project1","Project2","Project3"]
 
 styles = {
     "nav":{
-        "background-color":"rgba(123,209,146)",
+        "background-color":"pink",
+        "display": "flex",
+        "justify-content": "center"
     },
-    "div":{
-        "max-width":"32rem",
+    "img":{
+        "position": "absolute",
+        "left": "-20px",
+        "font-size": "15px",
+        "top": "4px",
+        "width": "100px",
+        "height": "40px",
     },
     "span":{
-        "border-radius":"0.5rem",
-        "color":"rgba(49,51,63)",
-        "margin":"0 0.125rem",
-        "padding":"0.4375rem 0.625rem",
+        "display": "block",
+        "color": "black",
+        "padding": "0.2rem 0.725rem",
+        "font-size": "14px",
     },
     "active":{
-        "background-color":"rgba(105,114,255,0.25)",
-    },
-    "hover":{
-        "background-color":"rgba(255,255,255,0.35)",
+        "background-color": "purple",
+        "color": "white",
+        "font-weight": "normal",
+        "padding": "14px",
     },
 }
 
+options = {
+    "show_menu": False,
+    "show_sidebar": True,
+}
 
-page = navbar(pages, styles=styles)
+page = navbar(pages, styles=styles, logo_path=logo_path,   options=options)
 
 if page == 'Home':
     Home.Home().app()
